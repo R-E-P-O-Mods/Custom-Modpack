0f44b75394c7099e03f5bcab93bc006eedc31f6c
Aleksandr <62847375+DANTECK-dev@users.noreply.github.com>
2025-05-16 21:04:32 +0800

    updated coment in the fucking auto updater . bat

--- update_mods.bat
+++ update_mods.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
-echo ==== Обновление модов из Git ====
+echo ==== We're starting to update the fucking R.E.P.O. mods via fucking GitHub. ====
 git pull origin master
 echo.
-echo ==== Завершено. Нажмите любую клавишу ==== 
+echo ==== Well all. fucking great. All mods are updated, you can play fucking R.E.P.O. ==== 
 pause >nul
