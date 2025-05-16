5b35d5acb9f4d5dbf17c646c7fb88635631aeb5d
Aleksandr <62847375+DANTECK-dev@users.noreply.github.com>
2025-05-16 21:06:38 +0800

    updated fucking auto update script

--- update_mods.bat
+++ update_mods.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
-echo ==== We're starting to update the fucking R.E.P.O. mods via fucking GitHub. ====
+echo <=====8 We're starting to update the fucking R.E.P.O. mods via fucking GitHub. 8=====>
 git pull origin master
 echo.
-echo ==== Well all. fucking great. All mods are updated, you can play fucking R.E.P.O. ==== 
+echo <=====8 Well all. fucking great. All mods are updated, you can play fucking R.E.P.O. 8=====> 
 pause >nul
